adffdb8d9d9d9c9c31e9af306843771883794940
Vitor mesquita <vitor.mesquita09@gmail.com>
2018-04-18 09:17:22 -0400

    refactoring folders

--- Source/Cells/Material/MaterialBaseRow/MaterialBaseRow.swift
+++ Source/Cells/Material/MaterialBaseRow/MaterialBaseRow.swift
@@ -12,7 +12,6 @@ import Eureka
 class MaterialBaseRow<Cell: CellType>: Row<Cell> where Cell: BaseCell {
     
     open var placeholder: String?
-    
     open var tintColor: UIColor? {
         didSet { cell.update() }
     }
